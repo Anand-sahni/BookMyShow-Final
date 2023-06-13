9c70553cb421c44ba0750235383497354744826b
Anand Sahni <anandsahni57@gmail.com>
2023-06-13 19:40:05 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express = require("express"); 
+const app = express(); 
+const { connection } = require("./connection"); 
+import connection from './conn'
+
+
+const cors = require("cors"); 
+const bodyParser = require("body-parser"); 
+const path = require('path');
+
+
+const PORT = process.env.PORT || 8080; 
+
+app.use(bodyParser.urlencoded({ extended: false })); 
+app.use(bodyParser.json()); 
+
+app.use(cors()); 
+
+connection(); 
+
+app.use("/api", require("./routes")); 
+
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}!`)); 
+
+module.exports = app;
