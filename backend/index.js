5191cdc426a36b0501050c04e6513739b71050c4
Anand Sahni <75078213+Anand-sahni@users.noreply.github.com>
2023-06-13 19:48:35 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express"); 
 const app = express(); 
 const { connection } = require("./connection"); 
-import connection from './conn'
+import connection from './connection'
 
 
 const cors = require("cors"); 
